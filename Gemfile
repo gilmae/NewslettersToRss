8c20b23787608049604f7f45ec90b948f62262af
gilmae <avocadia@fastmail.fm>
2020-07-04 17:26:40 +1000

    Script to pull my newsletter subscriptions from my mail and turn them into an RSS feed

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "mail", "~> 2.7"
+
+gem "rss", "~> 0.2.9"
---
+++ src/main.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+require 'net/imap'
+require 'mail'
+require 'rss'
+
+imap = Net::IMAP.new(ENV["IMAP_SERVER"], 993, true)
+imap.login(ENV["IMAP_UID"], ENV["IMAP_PWD"])
+imap.select(ENV["IMAP_FOLDER"])
+ids = imap.search("RECENT")
+(ids = imap.search(["SENTSINCE", (Time.now-7*60*60*24).strftime("%d-%b-%Y")])) if (ids.nil? || ids.size == 0)
+
+rss = RSS::Maker.make('rss2.0') do |maker|
+    maker.channel.title = "Newsletters"
+    maker.channel.link = ENV["BASEURL"]
+    maker.channel.description = "My newsletter subscriptions as an rss feed"
+
+    ids.each do |id|
+        imsg = imap.fetch(id, "RFC822")[0].attr["RFC822"]
+
+        message = Mail.read_from_string imsg
+
+        maker.items.new_item do |item|
+            item.link = "#{ENV["BASEURL"]}/item/#{id}"
+            item.title = message.Subject
+            
+            if message.multipart?
+                message.body.parts.each do |p|
+                    if (p.content_type.index("text/html") == 0)
+                        item.description = p.body.to_s.force_encoding('UTF-8')
+                    end
+                end
+            else
+                item.description = message.body.to_s.force_encoding('UTF-8')
+            end
+            item.author = message.from
+            item.updated = message.date.to_s
+        end
+    end
+end
+
+puts rss
