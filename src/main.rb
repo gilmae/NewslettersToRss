f0aac9d4c52aa75fda10d52799b5264e28a8a4f0
gilmae <avocadia@fastmail.fm>
2020-07-06 07:48:33 +1000

    Just use everything seen since a date, and sort and reverse it

--- src/main.rb
+++ src/main.rb
@@ -7,8 +7,8 @@ require 'rss'
 imap = Net::IMAP.new(ENV["IMAP_SERVER"], 993, true)
 imap.login(ENV["IMAP_UID"], ENV["IMAP_PWD"])
 imap.select(ENV["IMAP_FOLDER"])
-ids = imap.search("RECENT")
-ids.uniq!
+ids = imap.search(["SENTSINCE", (Time.now-7*60*60*24).strftime("%d-%b-%Y")]).sort.reverse
+
 
 rss = RSS::Maker.make('rss2.0') do |maker|
     maker.channel.title = "Newsletters"
